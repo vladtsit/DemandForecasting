228d4fd645c87505269a41ff9234d4647259e82b
karpova01 <101926190+karpova01@users.noreply.github.com>
2022-05-17 15:56:47 +0300

    Update install_requirements.sh

--- environment_setup/install_requirements.sh
+++ environment_setup/install_requirements.sh
@@ -25,7 +25,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 
-python --version
-pip install azure-cli==2.0.69
-pip install --upgrade azureml-sdk[cli]
+#python --version
+#pip install azure-cli==2.0.69
+#pip install --upgrade azureml-sdk[cli]
 pip install -r ./environment_setup/requirements.txt
